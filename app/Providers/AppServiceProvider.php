6b5e1056429205aaa0885436ee24cb859f60692e
Sergio <sergi0vargas@users.noreply.github.com>
2022-11-28 11:29:55 -0500

    imports

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (App::environment('production')) {
+        if (\App::environment('production')) {
             URL::forceScheme('https');
         }
     }
